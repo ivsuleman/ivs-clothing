9861bb8f3664e139dd820c60188d40c8a581f837
Irfan Suleman <ivsuleman@gmail.com>
2024-10-17 16:44:02 +0100

    added outlets for navigation component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+  return (
+    <div>
+      <h1>This is the Navigation Component</h1>
+      <Outlet />
+    </div>
+  );
+};
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>This is the Shop Component</h1>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="shop" element={<Shop />}></Route>
+      </Route>
     </Routes>
   );
 };
